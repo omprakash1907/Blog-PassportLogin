00ea949774f8d10bb4afbb5ff909eb0281c5e45f
omprakash1907 <omprakashkjat19@gmail.com>
2024-05-15 09:29:02 +0530

    added vercel file

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 var cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
+const nodemailer = require('nodemailer')
 const app = express()
 const fs = require('fs');
 
@@ -19,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 //-------------------multer
-const multer  = require('multer');
+const multer = require('multer');
 const { error } = require('console');
 
 const storage = multer.diskStorage({
@@ -53,7 +54,7 @@ app.use(flash());
 
 function auth(req, res, next) {
   if (req.isAuthenticated()) {
-      return next();
+    return next();
   }
   res.redirect('/');
 }
@@ -63,19 +64,19 @@ passport.use(new LocalStrategy({
   usernameField: 'email',
 },
   async function (email, password, done) {
-      try {
-          const user = await userModel.findOne({ email: email });
-          // console.log(user);
-          if (!user) {
-              return done(null, false, { message: 'Incorrect email.' });
-          }
-          if (user.password !== password) {
-              return done(null, false, { message: 'Incorrect password.' });
-          }
-          return done(null, user);
-      } catch (err) {
-          return done(err);
+    try {
+      const user = await userModel.findOne({ email: email });
+      // console.log(user);
+      if (!user) {
+        return done(null, false, { message: 'Incorrect email.' });
       }
+      if (user.password !== password) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
   }
 ));
 
@@ -86,10 +87,10 @@ passport.serializeUser((user, done) => done(null, user.id));
 
 passport.deserializeUser(async (id, cb) => {
   try {
-      const userData = await userModel.findById(id);
-      cb(null, userData);
+    const userData = await userModel.findById(id);
+    cb(null, userData);
   } catch (err) {
-      cb(err);
+    cb(err);
   }
 });
 
@@ -100,7 +101,7 @@ app.get('/', function (req, res) {
   res.render('./Pages/login');
 });
 
-app.post('/',  passport.authenticate('local', {
+app.post('/', passport.authenticate('local', {
   successRedirect: '/dashboard',
   failureRedirect: '/',
   failureFlash: true
@@ -116,42 +117,100 @@ app.post('/signup', async (req, res) => {
   res.redirect('/')
 })
 
+//---------------------------forgot pass
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: "omprakashkjat19@gmail.com",
+    pass: "btpp pwdx qsgz yrxn"
+  }
+});
+
+
+
+app.get('/forgot', async (req, res) => {
+  res.render('./Pages/forget')
+})
+
+app.post('/forgot', async (req, res) => {
+  const { email } = req.body
+  console.log(email)
+  var mailOptions = {
+    from: 'omprakashkjat19@gmail.com',
+    to: email,
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+  };
+
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+  res.redirect('/otp')
+})
+
 app.get('/dashboard', auth, async (req, res) => {
-  const post = await postModel.find({}) 
-  // const user = await userModel.find({})
+  const post = await postModel.find().sort('-1')
   const user = req.user.name
   res.render('./Pages/hero', { post: post, user: user });
 })
 
 app.post('/dashboard', auth, async (req, res) => {
   const user = req.user.name
-  upload(req, res, async function(){
-    if(req.file){
+  upload(req, res, async function () {
+    if (req.file) {
       var details = {
-        file : req.file.filename,
-        post : req.body.post,
-        time : Date.now(),
-        name : user
+        file: req.file.filename,
+        post: req.body.post,
+        time: Date.now(),
+        name: user
       }
       const post = await postModel(details)
       const result = post.save()
       res.redirect('/')
-    }else{
+    } else {
       console.log("error")
     }
   })
 })
 
-// logout
+//---------------- logout
 app.get('/signout', (req, res) => {
   req.logout(function (err) {
     if (err) {
-        console.error(err);
-        return res.redirect('/');
+      console.error(err);
+      return res.redirect('/');
     }
     res.redirect('/');
+  });
 });
-});
+
+//--------------myProfile
+
+app.get('/myprofile', auth, (req, res) => {
+  const user = req.user.name
+  res.render('./Pages/myprofile', { user: user })
+})
+
+app.post('/myprofile', auth, async (req, res) => {
+  const user = req.user.name
+  const password = req.user.password
+  const newpassword = req.body.newpassword
+  const renewpassword = req.body.renewpassword
+  console.log(req.body)
+  if (password === req.body.password) {
+    if (newpassword === renewpassword) {
+      var changePass = await userModel.updateOne({ _id: req.user.id }, { $set: { password: newpassword } })
+    }
+  } else {
+    console.log("errr")
+  }
+  res.render('./Pages/myprofile', { user: user })
+})
 
 
 app.listen((3000), () => {
